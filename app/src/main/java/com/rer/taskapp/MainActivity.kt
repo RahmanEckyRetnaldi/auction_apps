eb05a56652877ac108d2890d77dd46942e2c042f
RahmaneckyRetnaldy <rahman.ecky@akarinti.tech>
2024-09-28 18:45:20 +0700

    feat (app) : preview ui

--- app/src/main/java/com/rer/taskapp/MainActivity.kt
+++ app/src/main/java/com/rer/taskapp/MainActivity.kt
@@ -3,19 +3,13 @@ package com.rer.taskapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.rer.core.composable.input.GeneralInputData
-import com.rer.core.composable.input.GeneralInputOutlined
+import com.rer.auction.domain.entity.ChatEntity
+import com.rer.auction.presentation.auction.AuctionState
+import com.rer.auction.presentation.auction.ui.AuctionView
 import com.rer.core.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,29 +18,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val value = remember {
-                mutableStateOf("")
-            }
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                AuctionView(
+                    state = AuctionState().copy(
+                        liveChats = listOf(
+                            ChatEntity(
+                                user = "Rer",
+                                message = "Hello"
+                            ),
+                            ChatEntity(
+                                user = "Rer",
+                                message = "Hello"
+                            ),
+                            ChatEntity(
+                                user = "Rer",
+                                message = "Hello"
+                            )
+                        )
                     )
-                    GeneralInputOutlined(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        data = GeneralInputData(
-                            value = value.value,
-                            title = "Kalimat",
-                            placeholder = "Maukan kata",
-                            alert = "",
-                            onValueChange = {
-                                value.value = it
-                            },
-                        ),
-                    )
-                }
+                )
+
+
             }
         }
     }
