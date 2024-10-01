247e20a004c3793566ae50d3711c96df6439b30b
RahmaneckyRetnaldy <rahman.ecky@akarinti.tech>
2024-10-01 21:30:14 +0700

    feat (app) : remove unused component

--- app/src/main/java/com/rer/taskapp/MainActivity.kt
+++ app/src/main/java/com/rer/taskapp/MainActivity.kt
@@ -3,10 +3,6 @@ package com.rer.taskapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.rer.core.ui.theme.AppTheme
 import com.rer.core.utils.NavigationDestination
 import com.rer.taskapp.navigation.AppNavigationGraph
@@ -25,21 +21,5 @@ class MainActivity : ComponentActivity() {
                 AppNavigationGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
